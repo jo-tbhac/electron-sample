f89192ab10e6b844218fbe1068d9269d55699b53
junpei ogawa <cornerstone.am3@gmail.com>
2020-09-26 09:18:58 +0900

    set up fontawesome

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 
 import App from './components/App';
 import store from './store';
 import './index.scss';
+
+library.add(fab, fas, far);
 
 ReactDOM.render(
   <Provider store={store}>
